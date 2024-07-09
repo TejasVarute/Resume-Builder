085238c456986ece12827aa1ae5c1dd27763c9b7
Tejas Varute <65999219+TejasVarute@users.noreply.github.com>
2024-07-09 21:17:17 +0530

    Update web_gui.py

--- web_gui.py
+++ web_gui.py
@@ -1,33 +1,33 @@
 import streamlit as st
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-import io
-
 from resume import Resume
 
 class strealit_web_gui:
     def __init__(self):
         pass
     def data(self):
+        heading = """<h1 align="center">ATS FRIENDLY RESUME BUILDER</h1><br><br>"""
+        st.html(heading)
+
         st.header("PERSONAL INFORMATION")
         name = st.text_input('Your Name', placeholder='Enter Your Name')
         email = st.text_input('Your Email', placeholder='Enter Your Email')
         mobile = st.text_input('Your Mobile No.', placeholder='Enter Your Mobile Number')
         linkedin = st.text_input('Your Linkedin', placeholder='Enter Your Linkedin URL')
+        objective = st.text_input('Your Objective (2-3 lines)', placeholder='Enter Your Objective')
 
         st.header("EDUCATION INFORMATION")
         st.subheader("SSC")
         ssc_place = st.text_input('Your SSC college', placeholder='Enter Your 10th College Name')
-        ssc_adr = st.text_input('Your college address', placeholder='Enter Your 10th College Address')
-        ssc_year = st.date_input('Your SSC Year of Passing')
+        ssc_adr = st.text_input('Your college City', placeholder='Enter Your 10th College City')
+        ssc_year = st.text_input('Your SSC Year of Passing')
         ssc_result = st.text_input('Result', placeholder='Enter Your SSC Percentages')
         ssc_degree = "SSC"
 
         st.subheader("AFTER SSC")
         field = st.selectbox("Your Field after 10th", ["HSC", "DEPLOMA"])
         e2_place = st.text_input(f'Your {field} college', placeholder='Enter Your College Name')
-        e2_adr = st.text_input('Your college address', placeholder='Enter Your College Address')
-        e2_year = st.date_input(f'Your {field} Passing Year')
+        e2_adr = st.text_input('Your college City', placeholder='Enter Your College City')
+        e2_year = st.text_input(f'Your {field} Passing Year')
         e2_result = st.text_input('Result', placeholder=f'Enter Your {field} Percentages')
 
         st.subheader("Under Graduation Course")
@@ -39,9 +39,11 @@ class strealit_web_gui:
 
         if field1 == 'BTech':
             branch = st.selectbox("Select branch", branches)
+        else:
+            branch = ''
         e3_place = st.text_input(f'Your {field1} college', placeholder='Enter Your College Name')
-        e3_adr = st.text_input('Your college address', placeholder=f'Enter Your {field1} College Address')
-        e3_year = st.date_input(f'Your {field1} Passing Year')
+        e3_adr = st.text_input('Your college City', placeholder=f'Enter Your {field1} College City')
+        e3_year = st.text_input(f'Your {field1} Passing Year')
         e3_result = st.text_input('Result', placeholder=f'Enter Your {field1} Percentages / CGPA')
 
         st.header("SKILLS")
@@ -50,19 +52,21 @@ class strealit_web_gui:
         soft_skills = st.text_input('Enter Soft skills (use (comma , ) for saperation)', placeholder='Enter Soft Skills')
         lang = st.text_input('Enter Languages you speak (use (comma , ) for saperation)', placeholder='Enter languages Known')
         tools = st.text_input('Enter Computer Competencies You Know (use (comma , ) for saperation)', placeholder='Enter Computer competencies')
-        hobbie = st.text_input('Enter Hobbies (use (comma , ) for saperation)', placeholder='Enter Your Hobbies')
+        hobbie = st.text_input('Enter Hobbies (use (comma ,) for saperation) (Max 12)', placeholder='Enter Your Hobbies')
 
         st.header("PROJECTS")
         st.subheader("Project 1")
         proj1_title = st.text_input('Enter Title of Project 1)', placeholder='Enter Project 1 Title')
-        proj1_desc = st.text_input('Enter Description of Project 1)', placeholder='Enter Project 1 Description')
+        proj1_desc = st.text_input('Enter Description of Project 1 (4-5 lines))', placeholder='Enter Project 1 Description')
 
         st.subheader("Project 2")
         proj2_title = st.text_input('Enter Title of Project 2)', placeholder='Enter Project 2 Title')
-        proj2_desc = st.text_input('Enter Description of Project 2)', placeholder='Enter Project 2 Description')
+        proj2_desc = st.text_input('Enter Description of Project 2 (4-5 lines))', placeholder='Enter Project 2 Description')
 
         st.header("CERTIFICATIONS")
-        cert_count = st.text_input('How many Certificates you have', placeholder="How many certificates you have")
+        cert_count = st.text_input('How many Certificates you have (Max 8)', placeholder="How many certificates you have")
+        if cert_count and int(cert_count) > 8:
+            st.warning("Providing out of limit consider only 8")
         certificates = {}
         if cert_count:
             for i in range (int(cert_count)):
@@ -71,7 +75,15 @@ class strealit_web_gui:
                     k, v = cert.split(':')
                     certificates[k] = v
 
-        buffer = Resume().get_pdf() #name, email, mobile, linkedin, ssc_place, ssc_adr, ssc_year, ssc_result, ssc_degree, e2_place, e2_adr, field, e2_year, e2_result, e3_place, e3_adr, field1, branch, e3_year, e3_result, tech_skills, soft_skills, lang, tools, hobbie, proj1_title, proj1_desc, proj2_title, proj2_desc, certificates).get_pdf()
-        st.download_button(label="Resume Download", data=buffer, file_name="Resume.pdf", mime="application/pdf")
+        submitted = st.button("Submit all Information")
+        if submitted:
+            try:
+                buffer = Resume(name, email, mobile, linkedin, objective, ssc_place, ssc_adr, ssc_year, ssc_result, ssc_degree,
+                            e2_place, e2_adr, field, e2_year, e2_result, e3_place, e3_adr, field1, branch,
+                            e3_year, e3_result, tech_skills, soft_skills, lang, tools, hobbie, proj1_title,
+                            proj1_desc, proj2_title, proj2_desc, certificates).get_pdf()
+                st.download_button(label="Resume Download", data=buffer, file_name="Resume.pdf",mime="application/pdf")
+            except:
+                st.subheader(":red[Please Insert All information to get Resume]")
 
 strealit_web_gui().data()
